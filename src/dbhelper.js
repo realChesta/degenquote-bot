6491ae031d3f8e07ae0b5873fc7d7e2f5765c7e5
Chesta <chesta@protonmail.com>
2019-04-09 15:01:49 +0200

    User details are now refreshed on every quote

--- src/dbhelper.js
+++ src/dbhelper.js
@@ -56,6 +56,8 @@ class DbHelper {
                 quotes: 0
             };
 
+            this.users[userId].username = username;
+            this.users[userId].first_name = firstName;
             this.updateUserInDB(userId);
 
             return true;
