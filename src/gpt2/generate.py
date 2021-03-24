0d0029d43a03c7c968770e6b43e1088099a40aa7
Stan Wohlwend <n2d4xc@gmail.com>
2021-03-24 16:53:03 +0100

    Truncate GPT-2 output

--- src/gpt2/generate.py
+++ src/gpt2/generate.py
@@ -5,7 +5,7 @@ sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess)
 
 print("Generating GPT-2...", file=sys.stderr, flush=True)
-list = gpt2.generate(sess, prefix=sys.argv[1] + "\n\n", include_prefix=False, return_as_list=True)
+list = gpt2.generate(sess, prefix=sys.argv[1] + "\n\n", include_prefix=False, truncate="<|endoftext|>", return_as_list=True)
 print("Generated GPT-2!", file=sys.stderr, flush=True)
 
 for str in list:
